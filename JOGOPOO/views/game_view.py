59da8be9ee10490b9a4d6fbbf3b7422d7b9f87d7
Silvia H Santos Henrique dos santos <LucasHenrique>
2026-07-29 19:32:53 -0300

    atualização

--- JOGOPOO/views/game_view.py
+++ JOGOPOO/views/game_view.py
@@ -23,34 +23,46 @@ class GameView(arcade.View):
 
         # Jogador
         self.player_list = arcade.SpriteList()
+
         self.player = Player()
         self.player_list.append(self.player)
 
         # Vitaminas
         self.vitamina_list = arcade.SpriteList()
 
-        for _ in range(10):
-            self.vitamina_list.append(Vitamina())
+        for i in range(6):
+
+            while True:
+                vitamina = Vitamina()
+
+                # Só adiciona se não estiver encostando no jogador
+                if not arcade.check_for_collision(vitamina, self.player):
+                    self.vitamina_list.append(vitamina)
+                    break
 
         # Pontuação
         self.pontos = 0
 
     def on_draw(self):
+
         self.clear()
 
         self.background_list.draw()
+
         self.vitamina_list.draw()
+
         self.player_list.draw()
 
         arcade.draw_text(
-            f"Pontos: {self.pontos}",
+            f"Vitaminas: {self.pontos}",
             20,
-            ALTURA - 30,
+            560,
             arcade.color.WHITE,
             20
         )
 
     def on_update(self, delta_time):
+
         self.player_list.update()
 
         vitaminas = arcade.check_for_collision_with_list(
